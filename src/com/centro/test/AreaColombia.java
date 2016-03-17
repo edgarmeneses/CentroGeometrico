17ee26d96113b7efd793faa2fb770fc3e5e5f296
AntoniusHack <AntoniusHack@AntoniusHack-PC>
2016-03-16 20:23:21 -0500

    proyecto

---
+++ src/com/centro/test/AreaColombia.java
@@ -0,0 +1,12 @@
+package com.centro.test;
+
+import com.centro.gui.MainFrame;
+
+public class AreaColombia {
+
+	public static void main(String[] args) {
+		MainFrame mf = new MainFrame();
+		mf.setVisible(true);
+
+	}
+}
