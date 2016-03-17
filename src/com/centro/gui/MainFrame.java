dc55bf610be2e9c1ad9f45f1d38d1ab2b82bbb88
AntoniusHack <AntoniusHack@AntoniusHack-PC>
2016-03-16 20:23:21 -0500

    proyecto

---
+++ src/com/centro/gui/MainFrame.java
@@ -0,0 +1,390 @@
+package com.centro.gui;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Color;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
+import java.util.prefs.BackingStoreException;
+
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import com.centro.logica.ImageArea;
+
+import java.awt.Font;
+
+public class MainFrame extends JFrame implements ChangeListener {
+
+	JSpinner tolerancia;
+	ImageArea image;
+	JPanel filter;
+	JTextField puntos;
+	JTextField ancho;
+	JTextField experimentos;
+	JLabel result;
+	JLabel  label;
+	private JLabel lblResultadoCentroMedio;
+	private JLabel lblX;
+	private JLabel lblResultadoX;
+	private JLabel lblY;
+	private JLabel lblResultadoY;
+	private JLabel lblImgAcerca;
+	private JLabel lblAcercaDe;
+	private JLabel lblImgGenerar;
+	private JLabel lblGenarar;
+	private JLabel lblImgSalir;
+	private JLabel lblSalir;
+	private JLabel lblImgencargarImagen;
+	private JLabel lblCargarImagen;
+	private JLabel lblSalir_1;
+
+
+	public MainFrame() throws HeadlessException {
+		init();
+		setVisible(true);
+	}
+
+	private void init(){
+		setTitle("Centro Geometrico");
+		this.setSize(974,654);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		this.setResizable(false);
+		ImageIcon icono = new ImageIcon(getClass().getResource("../img/icono.png")); 
+		this.setIconImage(icono.getImage());
+
+		JLabel lbl_tolerancia = new JLabel("Tolerancia:");
+		lbl_tolerancia.setBounds(10,558,100,30);
+		getContentPane().add(lbl_tolerancia);
+
+		tolerancia = new JSpinner();
+		tolerancia.addChangeListener(this);
+		tolerancia.setBounds(99, 558, 100, 30);
+		tolerancia.setValue(75);
+		getContentPane().add(tolerancia);
+
+		filter = new JPanel(null);
+		filter.setBounds(10, 11, 520, 520);
+		filter.setBorder(new TitledBorder("Imagen"));
+		getContentPane().add(filter);
+
+
+		result=new JLabel("Resultado:");
+		result.setBounds(540, 279, 418, 30);
+		getContentPane().add(result);
+
+		JLabel lbl_puntos = new JLabel("Puntos:");
+		lbl_puntos.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lbl_puntos.setBounds(540,27,80,30);
+		getContentPane().add(lbl_puntos);
+
+		puntos = new JTextField("100");
+		puntos.setBounds(651,27,100,30);
+		getContentPane().add(puntos);
+
+		JLabel lbl_ancho = new JLabel("Ancho:");
+		lbl_ancho.setBounds(209,558,80,30);
+		getContentPane().add(lbl_ancho);
+
+		ancho = new JTextField();
+		ancho.setBounds(289,558,100,30);
+		getContentPane().add(ancho);
+
+		JLabel lbl_expermientos = new JLabel("Experimentos:");
+		lbl_expermientos.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lbl_expermientos.setBounds(540,68,100,30);
+		getContentPane().add(lbl_expermientos);
+
+		experimentos = new JTextField();
+		experimentos.setBounds(651,70,100,30);
+		getContentPane().add(experimentos);
+
+		lblResultadoCentroMedio = new JLabel("Resultado Centro Medio");
+		lblResultadoCentroMedio.setForeground(new Color(139, 0, 0));
+		lblResultadoCentroMedio.setFont(new Font("Aharoni", Font.BOLD | Font.ITALIC, 16));
+		lblResultadoCentroMedio.setBounds(540, 207, 211, 30);
+		getContentPane().add(lblResultadoCentroMedio);
+
+		lblX = new JLabel("X: ");
+		lblX.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblX.setBounds(540, 248, 46, 14);
+		getContentPane().add(lblX);
+
+		lblResultadoX = new JLabel("Resultado x");
+		lblResultadoX.setBounds(566, 247, 127, 21);
+		getContentPane().add(lblResultadoX);
+
+		lblY = new JLabel("Y:");
+		lblY.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblY.setBounds(750, 248, 46, 14);
+		getContentPane().add(lblY);
+
+		lblResultadoY = new JLabel("Resultado y");
+		lblResultadoY.setBounds(782, 247, 127, 21);
+		getContentPane().add(lblResultadoY);
+
+		lblCargarImagen = new JLabel("Cargar Imagen");
+		lblCargarImagen.setBounds(673, 502, 88, 14);
+		getContentPane().add(lblCargarImagen);
+
+		lblImgencargarImagen = new JLabel("");
+		lblImgencargarImagen.setBounds(679, 437, 60, 60);
+		getContentPane().add(lblImgencargarImagen);
+		ImageIcon icon2 = new ImageIcon(this.getClass().getResource("../img/cargar.png"));
+		lblImgencargarImagen.setIcon(icon2);
+		lblImgencargarImagen.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				JFileChooser chooser= new JFileChooser();
+				FileNameExtensionFilter fn= new FileNameExtensionFilter("JPG & PNG Images", "jpg", "png");
+				chooser.setFileFilter(fn);
+				int returnVal = chooser.showOpenDialog(lblCargarImagen);
+				
+			    if(returnVal == chooser.APPROVE_OPTION) {
+			       System.out.println("Nombre: " + chooser.getSelectedFile().getName());
+			       try {
+					image= new ImageArea(chooser.getSelectedFile());
+			    	   update();
+				} catch (IOException e1) {
+					JOptionPane.showMessageDialog(lblCargarImagen, "Error al cargar la imagen");
+				}
+				if (chooser.getSelectedFile().exists()) {
+						System.out.println("Si sirveeeee");
+					}else{
+						System.out.println("No seirviiooo");
+					}
+						
+			    }
+			}
+
+		});
+
+		lblAcercaDe = new JLabel("Acerca De");
+		lblAcercaDe.setBounds(567, 502, 60, 14);
+		getContentPane().add(lblAcercaDe);
+
+		lblImgAcerca = new JLabel();
+		lblImgAcerca.setBounds(567, 432, 60, 60);
+		getContentPane().add(lblImgAcerca);
+		ImageIcon icon1 = new ImageIcon(this.getClass().getResource("../img/acercade.png"));
+		lblImgAcerca.setIcon(icon1);
+
+		lblGenarar = new JLabel("Ejecutar");
+		lblGenarar.setBounds(821, 97, 60, 14);
+		getContentPane().add(lblGenarar);
+
+		lblImgGenerar = new JLabel();
+		lblImgGenerar.setBounds(811, 27, 60, 60);
+		getContentPane().add(lblImgGenerar);
+		ImageIcon icon3 = new ImageIcon(this.getClass().getResource("../img/ejecutar.png"));
+		lblImgGenerar.setIcon(icon3);
+		lblImgGenerar.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				calcular();
+				System.out.println("Esta ejecutandooo");
+				
+			}
+		});
+
+		lblSalir_1 = new JLabel("Salir");
+		lblSalir_1.setBounds(825, 502, 46, 14);
+		getContentPane().add(lblSalir_1);
+		
+		lblImgSalir = new JLabel();
+		lblImgSalir.setBounds(798, 433, 60, 60);
+		getContentPane().add(lblImgSalir);
+		ImageIcon icon4 = new ImageIcon(this.getClass().getResource("../img/salir.png"));
+		lblImgSalir.setIcon(icon4);
+		lblImgSalir.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				int option = JOptionPane.showConfirmDialog(null, "¿Seguro que desea salir?", "salida",  JOptionPane.YES_NO_OPTION);
+				if(option==0){
+					System.exit(0);
+				}
+			}
+		});
+	}
+	public void update(){
+
+		double factor=0;
+		if ((double)(500.0/(double)image.getImage().getHeight()) <(double)(500.0/(double)image.getImage().getWidth())){
+			factor=(double)(500.0/(double)image.getImage().getHeight());
+		}else{
+			factor=(double)(500.0/(double)image.getImage().getWidth());
+		}
+		int h = (int)(factor * (double)image.getImage().getHeight());
+		int w = (int)(factor * (double)image.getImage().getWidth());
+
+		image.blackAndWhite(Double.parseDouble(tolerancia.getValue().toString()));
+
+		JLabel lbl_filter=new JLabel();
+		lbl_filter.setIcon(new ImageIcon(image.getImageResult().getScaledInstance(w,h, 100)));
+		lbl_filter.setBounds(10,10,w,h);
+		filter.removeAll();
+		filter.repaint();
+		filter.add(lbl_filter);
+		filter.repaint();
+
+		repaint();
+	}
+
+	public void calcular(){
+
+		update();
+		image.clear();
+		int exp = Integer.parseInt(experimentos.getText());
+		int cant_puntos = Integer.parseInt(puntos.getText());
+		double area_acum=0;
+		for (int e = 0; e < exp; e++) {
+			int acum = 0;
+			for (int i = 0; i < cant_puntos; i++) {
+				if(image.randomPoint()){
+					acum++;
+				}
+			}
+			area_acum+=(double)acum/(double)cant_puntos;
+
+		}
+
+		area_acum = area_acum/cant_puntos;
+
+		double ancho_img = Double.parseDouble(ancho.getText());
+		double alto_img = (ancho_img * (double)image.getImage().getWidth()) /  (double)image.getImage().getHeight();
+
+
+		result.setText("Resultado, Area: "+area_acum*(ancho_img *alto_img));
+
+
+		double factor=0;
+		if ((double)(500.0/(double)image.getImage().getHeight()) <(double)(500.0/(double)image.getImage().getWidth())){
+			factor=(double)(500.0/(double)image.getImage().getHeight());
+		}else{
+			factor=(double)(500.0/(double)image.getImage().getWidth());
+		}
+		int h = (int)(factor * (double)image.getImage().getHeight());
+		int w = (int)(factor * (double)image.getImage().getWidth());
+		JLabel lbl_filter=new JLabel();
+		lbl_filter.setIcon(new ImageIcon(image.getImageResult().getScaledInstance(w,h, 100)));
+		lbl_filter.setBounds(10,10,w,h);
+
+		label = new JLabel("Hol");
+		label.setForeground(Color.MAGENTA);
+		label.setBounds(275, 281, 20, 20);
+
+		int x = image.getCoordinateX();
+		int y =image.getCoordinateY();
+		//image.center(x,y );
+		System.out.println("x= "+ x+" y= "+y);
+
+		filter.removeAll();
+
+		//int x = image.getCoordinateX();
+		//int y =image.getCoordinateY();
+		//image.center(x,y );
+		//System.out.println("x= "+ x+" y= "+y);
+		filter.repaint();
+		filter.add(label);
+		filter.add(lbl_filter);
+		filter.repaint();
+		repaint();
+
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		if(e.getSource().equals(tolerancia)){
+
+		}
+	}
+}
---
+++ src/com/centro/logica/ImageArea.java
@@ -0,0 +1,124 @@
+package com.centro.logica;
+
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+import com.statistics.logic.Statistics;
+
+public class ImageArea {
+	
+	BufferedImage image;
+    BufferedImage imageResult;
+    int BLACK_RGB=-16777216;
+    int WHITE_RGB=-1;
+    File input;
+    ArrayList<Double> coordendasX;
+    ArrayList<Double> coordenadasY;
+
+    public ImageArea(File input) throws IOException {
+        image=ImageIO.read(input);
+        imageResult=ImageIO.read(input);
+        this.input = input;
+        this.coordenadasY = new ArrayList<Double>();
+        this.coordendasX = new ArrayList<Double>();
+        
+    }
+    public void blackAndWhite(double porcentaje){
+        int aux;
+        try {
+            setImageResult(ImageIO.read(input));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        porcentaje = (255.0/100.0)*porcentaje;
+        for (int i = 0; i < imageResult.getWidth(); i++) {
+            for (int j = 0; j < imageResult.getHeight(); j++) {
+                Color c=new Color(imageResult.getRGB(i, j));
+                aux=(c.getRed()+c.getGreen()+c.getBlue())/3;
+                
+                if(aux>porcentaje)imageResult.setRGB(i, j, -1);
+                else imageResult.setRGB(i, j, BLACK_RGB);
+            }
+        }
+    }
+    
+    public boolean randomPoint(){
+        int x = (int)(Math.random() * (double) imageResult.getWidth());
+        int y = (int)(Math.random() * (double) imageResult.getHeight());
+        boolean intersects = false;
+        if(imageResult.getRGB(x,y) == BLACK_RGB){
+            imageResult.setRGB(x, y, Color.ORANGE.getRGB());
+            
+            if(x>0 && y > 0 && x<imageResult.getWidth() && y <imageResult.getHeight() ){
+                imageResult.setRGB(x-1, y, Color.ORANGE.getRGB());
+                imageResult.setRGB(x-1, y-1, Color.ORANGE.getRGB());
+                imageResult.setRGB(x, y-1, Color.ORANGE.getRGB());
+            }
+            
+            coordendasX.add((double)x);
+            coordenadasY.add((double)y);
+            intersects = true;
+        }else{
+            imageResult.setRGB(x, y, Color.BLUE.getRGB());
+            
+            if(x>0 && y > 0 && x<imageResult.getWidth() && y <imageResult.getHeight() ){
+                imageResult.setRGB(x-1, y, Color.BLUE.getRGB());
+                imageResult.setRGB(x-1, y-1, Color.BLUE.getRGB());
+                imageResult.setRGB(x, y-1, Color.BLUE.getRGB());
+            }
+        }
+       
+        //center(getCoordinateX(), getCoordinateY());
+        return intersects;
+    }
+    
+    public void center(int x, int y){
+    	System.out.println(x+","+y);
+	//int [] rgb = {Color.MAGENTA.getRGB()};
+    	for (int i = x-100; i < x+100; i++) {
+			for (int j = y-100; j < y+100; j++) {
+				imageResult.setRGB(x, y, Color.magenta.getRGB());
+			}
+		}
+    	 
+    	 //imageResult.setRGB(x, y, 50, 50, rgb, 1, 1);
+    }
+    
+    public void clear(){
+    	coordenadasY.removeAll(coordenadasY);
+    	coordendasX.removeAll(coordendasX);
+    }
+    
+    
+    public int getCoordinateY(){
+    	return (int) (Statistics.average(coordenadasY));
+    }
+    
+    public int getCoordinateX(){
+    	return (int) (Statistics.average(coordendasX));
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    public BufferedImage getImageResult() {
+        return imageResult;
+    }
+
+    public void setImageResult(BufferedImage imageResult) {
+        this.imageResult = imageResult;
+    }
+
+
+}
